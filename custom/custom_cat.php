04100af78662105d46e718c996f7b971a2c2fbb2
potseluev <potseluev@inbox.ru>
2016-02-23 09:26:19 +0300

    Create custom_cat.php

---
+++ custom/custom_cat.php
@@ -0,0 +1,23 @@
+<?php
+    $nav['napolnye'] = array(
+        'xpath' => "//product[series='PN' and not(contains(model, '0Н'))]",
+        'caption' => "Напольные инверторы",
+        'title' => "Напольные инверторы",
+        'keywords' => "Напольные инверторы",
+        'description' => ""
+     );
+    $nav['nastennye'] = array(
+        'xpath' => "//product[series='PN' and contains(model, '0Н')]",
+        'caption' => "Настенные инверторы",
+        'title' => "Настенные инверторы",
+        'keywords' => "Настенные инверторы",
+        'description' => ""
+     );
+    $nav['automobile'] = array(
+        'xpath' => "//product[series='AL']",
+        'caption' => "Автомобильные инверторы",
+        'title' => "Автомобильныеые инверторы",
+        'keywords' => "Автомобильные инверторы",
+        'description' => ""
+     );
+?>
